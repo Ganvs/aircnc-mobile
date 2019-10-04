541a7e2517cb34ef82561e236effb81c10039756
Ganvs <gustavo.anverze@gmail.com>
2019-10-04 20:50:53 -0300

    SpotList component structure

--- src/components/SpotList.js
+++ src/components/SpotList.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  FlatList,
+  Image,
+  TouchableOpacity
+} from "react-native";
 import api from "../services/api";
 
 export default function SpotList({ tech }) {
@@ -17,5 +24,51 @@ export default function SpotList({ tech }) {
     loadSpots();
   }, []);
 
-  return <Text>{tech}</Text>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Empresas que usam <Text style={styles.bold}>{tech}</Text>
+      </Text>
+
+      <FlatList
+        style={styles.list}
+        data={spots}
+        keyExtractor={spot => spot._id}
+        horizontal
+        showsHorizontalScrollIndicator={true}
+        renderItem={({ item }) => (
+          <View style={styles.listItem}>
+            <Image
+              style={styles.thumbnail}
+              source={{ uri: item.thumbnail_url }}
+            />
+            <Text style={styles.company}>{item.company}</Text>
+            <Text style={styles.price}>
+              {item.price ? `R$${item.price}/dia` : "GRATUITO"}
+            </Text>
+            <TouchableOpacity onPress={() => {}} style={styles.button}>
+              <Text style={styles.buttonText}>Solicitar reserva</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 30
+  },
+
+  title: {
+    fontSize: 20,
+    color: "#444",
+    paddingHorizontal: 20,
+    marginBottom: 15
+  },
+
+  bold: {
+    fontWeight: "bold"
+  }
+});
